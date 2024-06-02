f4225c90c9c95031ab41f8d5426a9e97e5e176a1
Wectornanime <81574689+Wectornanime@users.noreply.github.com>
2024-06-02 19:11:06 -0300

    finished map screen

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,9 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+
 
 export default function Map() {
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [region, setRegion] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('A permissão para acessar o local foi negada!');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+            setRegion({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            });
+        })();
+    }, []);
+
+    let text = 'Aguardando..';
+    if (errorMsg) {
+        text = errorMsg;
+    } else if (location) {
+        text = JSON.stringify(location);
+    }
+
     return (
         <View style={styles.container}>
-            <Text>Hey, this Map is running!</Text>
+            <MapView style={styles.map} region={region}>
+                {location && (
+                    <Marker
+                        coordinate={{
+                            latitude: location.coords.latitude,
+                            longitude: location.coords.longitude,
+                        }}
+                        title="Você está aqui"
+                    />
+                )}
+            </MapView>
         </View>
     );
 }
@@ -14,5 +58,9 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    map: {
+        width: '100%',
+        height: '100%',
     },
 });
