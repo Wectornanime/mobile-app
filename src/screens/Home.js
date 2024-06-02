42ff833a93971d56d8293ea384ce9cfdb8ede0de
Wectornanime <81574689+Wectornanime@users.noreply.github.com>
2024-06-02 13:04:30 -0300

    finished home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,53 @@
 import { Button, StyleSheet, Text, View } from 'react-native';
 
+import * as Location from 'expo-location';
+import { useEffect, useState } from 'react';
+
 export default function Home({ navigation }) {
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [address, setAddress] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('A permissão para acessar o local foi negada!');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync();
+            setLocation(location);
+
+            let address = await Location.reverseGeocodeAsync(location.coords);
+            setAddress(address);
+        })();
+    }, []);
+
+    let locationText = 'Aguardando..';
+    let addressText = 'Aguardando..';
+
+    if (address) {
+        addressText = JSON.stringify(address[0].formattedAddress);
+    }
+
+    if (errorMsg) {
+        locationText = errorMsg;
+    } else if (location) {
+        locationText = `Latitude:${location.coords.latitude}\nLongetude:${location.coords.longitude}`;
+    }
+
     return (
         <View style={styles.container}>
-            <Text>Hey, this Home is running!</Text>
+            <Text style={styles.title}>Essa é a sua geolocalização:</Text>
+            <Text>{locationText}</Text>
+            <Text style={styles.title}>Esse é o seu endereço:</Text>
+            <Text>{addressText}</Text>
+
             <Button
-                title='Map'
+                style={styles.button}
+                title='Ver no mapa'
                 onPress={() => navigation.navigate('Map')}
             />
         </View>
@@ -18,5 +60,9 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        gap: 10,
+    },
+    title: {
+        fontSize: 20,
     },
 });
